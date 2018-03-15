592bd91a012704ab61b311beb5aa4a0459d08434
Ajeya Vempati <ajeya@macro.io>
2018-03-15 11:04:22 +1100

    Remove logo.png and change README.md when bootstrapping project

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-echo "Creating a new git repo"
+echo Boostrapping project
+if test -f logo.png; then
+  rm logo.png
+fi
+PROJECT=`pwd | sed 's#.*/##'`
+echo Readme for $PROJECT > README.md
+
+echo Creating a new git repo
 if test -d .git; then
-  echo "Repo already exists. Skipping ..."
+  echo Repo already exists. Skipping ...
 else
   echo Setting up git...
   git init
-  git add .
-  git commit -m "Initial commit"
 fi
 
-echo "Installing Dependencies ..."
+echo Installing Dependencies ...
 yarn install
-echo "Done."
+echo Done.
